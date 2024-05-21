5986df3a61163261102a553aaf772c66ef5db4c9
d2nb <824331661@qq.com>
2024-05-21 22:30:20 +0800

    reset css

--- src/views/home.vue
+++ src/views/home.vue
@@ -4,5 +4,8 @@
   <main>
     <h1 className="text-3xl font-bold underline">Hello world!</h1>
     <a-button type="primary">Primary Button</a-button>
+    <ul class="list-none p-0 m-0">
+      <li v-for="item in [1, 2, 3]" :key="item">{{ item }}</li>
+    </ul>
   </main>
 </template>
